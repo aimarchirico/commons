561d30f9aa1cff9b0d833b2054cc63e35935bf8a
Aimár A. Chirico <aimar@chirico.no>
2026-07-16 14:27:37 +0000

    chore: isolate configurations strictly in tools directory

---
+++ tools/eslint.config.ts
@@ -0,0 +1,35 @@
+let tsConfig;
+let toolsConfig;
+
+try {
+  tsConfig = (await import('@aimarchirico/commons-ts/eslint')).default;
+  toolsConfig = (await import('@aimarchirico/commons-tools/eslint')).default;
+} catch {
+  tsConfig = (await import('../npm/packages/commons-ts/eslint.ts')).default;
+  toolsConfig = (await import('../npm/packages/commons-tools/eslint.ts'))
+    .default;
+}
+
+export default [
+  ...tsConfig,
+  ...toolsConfig,
+  {
+    ignores: [
+      '../npm/packages/**/*',
+      '../npm/apps/**/*',
+      '../backend/**/*',
+      '../frontend/**/*',
+      '../**/.turbo/**',
+      '../**/Taskfile.yml',
+      '../**/pnpm-lock.yaml',
+      '../**/commitlint.config.js',
+    ],
+  },
+  {
+    files: ['**/eslint.config.ts'],
+    rules: {
+      'import/no-default-export': 'off',
+      'check-file/filename-naming-convention': 'off',
+    },
+  },
+];
