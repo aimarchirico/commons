633e73e6eb6ddfe09f36f925701f1eb510e8d2df
Aimár A. Chirico <aimar@chirico.no>
2026-07-17 22:17:02 +0200

    refactor(eslint): simplify eslint configs and standardize tsconfig includes

--- npm/packages/commons-cloudflare/eslint.config.ts
+++ npm/packages/commons-cloudflare/eslint.config.ts
@@ -1,3 +1,2 @@
 import baseConfig from '@aimarchirico/commons-ts/eslint';
-
 export default baseConfig;
--- npm/packages/commons-google-signin/eslint.config.ts
+++ npm/packages/commons-google-signin/eslint.config.ts
@@ -1,10 +1,2 @@
-import expoConfig from 'eslint-config-expo/flat.js';
-import baseConfig from '@aimarchirico/commons-ts/eslint';
-
-const dedupedConfig = baseConfig.map(config => {
-  if (!config?.plugins) return config;
-  const { '@typescript-eslint': _tsPlugin, import: _importPlugin, ...plugins } = config.plugins;
-  return {...config, plugins};
-});
-
+import baseConfig from '@aimarchirico/commons-expo/eslint';
 export default baseConfig;
