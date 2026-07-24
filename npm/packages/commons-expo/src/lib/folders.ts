1979bec9d0da74b5bb94d93b8848d4905f01836e
Aimár A. Chirico <aimar@chirico.no>
2026-07-24 12:58:28 +0000

    chore: fix style

--- npm/packages/commons-expo/src/lib/folders.ts
+++ npm/packages/commons-expo/src/lib/folders.ts
@@ -1,17 +1,7 @@
 import {CORE_FOLDERS as TS_FOLDERS} from '@aimarchirico/commons-ts/folders';
 
-export const CORE_FOLDERS = [
-  ...TS_FOLDERS,
-  'contexts',
-  'hooks',
-  'locales',
-];
+export const CORE_FOLDERS = [...TS_FOLDERS, 'contexts', 'hooks', 'locales'];
 
-export const UI_FOLDERS = [
-  ...CORE_FOLDERS,
-  'components',
-  'screens',
-  'styles',
-];
+export const UI_FOLDERS = [...CORE_FOLDERS, 'components', 'screens', 'styles'];
 
 export const APP_FOLDERS = ['app', 'assets', 'lib'];
